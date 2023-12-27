f6ceb949119f5733148e851e6d3f0a784e8e0136
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 12:43:27 +0000

    [FE] - Updated Container component.

--- fe-app/src/components/layout/Container.tsx
+++ fe-app/src/components/layout/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Container = ({ children }: Props) => {
   return (
-    <div className="container h-screen mx-auto flex flex-col px-4 mt-12 gap-14">
+    <div className="container h-screen mx-auto flex flex-col px-4 gap-14">
       {children}
     </div>
   );
