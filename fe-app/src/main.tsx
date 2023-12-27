f5e40df03ba6f2a2de0f2424e6df95f066ebb46f
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 12:24:58 +0000

    [FE] - Added react-router-dom library.

--- fe-app/src/main.tsx
+++ fe-app/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/components/App";
 import "./styles/main.css";
+
+import { MyRouter } from "@/components/my-router/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MyRouter />
   </React.StrictMode>
 );
