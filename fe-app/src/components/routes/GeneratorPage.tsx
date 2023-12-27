3788effc9600c3d8dc9237c08a2297d62283b7e9
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 12:25:23 +0000

    [FE] - Renamed file.

--- fe-app/src/components/routes/GeneratorPage.tsx
+++ fe-app/src/components/routes/GeneratorPage.tsx
@@ -1,16 +1,15 @@
-import Container from "@/components/layout/Container";
 import Header from "@/components/generator-page/header/index.tsx";
 import Grid from "@/components/generator-page/grid/index.tsx";
 import Footer from "@/components/generator-page/footer/index.tsx";
 
-function App() {
+function GeneratorPage() {
   return (
-    <Container>
+    <>
       <Header />
       <Grid />
       <Footer />
-    </Container>
+    </>
   );
 }
 
-export default App;
+export default GeneratorPage;
