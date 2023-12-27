30396fec70fcaf117b776e4ceaa4f38349054812
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 14:47:38 +0000

    [BE] - Created add payments route.

--- be-app/src/routes/api/exercise/add-payment.ts
+++ be-app/src/routes/api/exercise/add-payment.ts
@@ -6,19 +6,19 @@ import { v4 as uuidv4 } from 'uuid';
 const addPaymentRoute = Router();
 
 addPaymentRoute.post('/payments', (req, res) => {
-  const { payment } = req.body;
+  const { name, ammount } = req.body;
 
   const newPayment: Payment = {
     id: uuidv4(),
-    name: payment.name,
-    ammount: payment.ammount,
+    name: name,
+    ammount: ammount,
     grid: getMatrix(),
     code: getCode()
   };
 
   if (addPayment(newPayment)) {
     res.status(201);
-    return res.json({ message: 'Payment added succefully' });
+    return res.json({ status: 'added', message: 'Payment added succefully' });
   }
 
   res.status(409);
