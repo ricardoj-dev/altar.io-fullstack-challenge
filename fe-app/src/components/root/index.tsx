8f57a0a2f470e0ee0046c7e9550285277fbe3593
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 12:43:12 +0000

    [FE] - Created NavBar & NavBarLink components & added to Root component.

--- fe-app/src/components/root/index.tsx
+++ fe-app/src/components/root/index.tsx
@@ -1,10 +1,12 @@
 import Container from "@/components/layout/Container";
+import NavBar from "@/components/layout/NavBar";
 
 import { Outlet } from "react-router-dom";
 
 function Root() {
   return (
     <Container>
+      <NavBar />
       <Outlet />
     </Container>
   );
