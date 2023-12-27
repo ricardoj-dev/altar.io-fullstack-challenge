cdcdc4768a386ab5a903d0275cdc46591f85bb52
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 14:12:55 +0000

    [BE] - Added payments routes.

--- be-app/src/routes/api/exercise/index.ts
+++ be-app/src/routes/api/exercise/index.ts
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import { ApiRouter } from '@/types/api.types';
 import generateCodeRoute from './generate-code';
 import generateMatrixRoute from './generate-matrix';
+import addPaymentRoute from './add-payment';
+import getPaymentsRoute from './get-payments';
 
 const router = Router();
 
 // - Exercise 1 Routes
 router.use(generateCodeRoute);
 router.use(generateMatrixRoute);
+router.use(addPaymentRoute);
+router.use(getPaymentsRoute);
 
 const exerciseRoutes: ApiRouter = {
   router: router,
