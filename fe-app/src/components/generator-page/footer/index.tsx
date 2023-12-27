3ee03b9fd8e8a1e3f5e7cddb5817d1a4dc242b58
Ricardo <ricardosadeveloper@gmail.com>
2023-12-27 11:52:18 +0000

    [FE] - Created Footer component.

---
+++ fe-app/src/components/generator-page/footer/index.tsx
@@ -0,0 +1,19 @@
+import { useFullStackChallengeStore } from "@/store";
+
+const Footer = () => {
+  const code = useFullStackChallengeStore((state) => state.code);
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="flex justify-center items-center gap-1">
+        <div className="red-ball" />
+        <p className="text-blue-950 font-bold">Live</p>
+      </div>
+      <p className="border-2 border-blue-950 text-xl uppercase py-4 px-8">
+        Your Code: <span className="font-bold">{code}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
